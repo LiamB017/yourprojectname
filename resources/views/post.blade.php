cb5b08a27a14d1aab4d7188145c99fe5301d8846
LiamB017 <liambedford17@gmail.com>
2021-11-04 11:39:31 +0000

    I created the laravel project and began working with the laracasts to create a blog

---
+++ resources/views/post.blade.php
@@ -0,0 +1,20 @@
+<html>
+    <title>My blog</title>
+    <link rel="stylesheet" href="/app.css">
+    <script src="/app.js"></script>
+    <body>
+        
+    
+    <article>
+    <h1><a href="/post">My First Post</a></h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique dignissimos excepturi itaque et.
+         Sint alias voluptas ipsa consectetur cum, nisi saepe, culpa eligendi delectus pariatur totam incidunt dignissimos molestias, illo quas sunt unde quis itaque?
+         Laudantium fugiat aliquam quas autem molestias voluptate? Aliquam harum ab animi recusandae. Eveniet, eos modi.</p>
+    </article>
+
+   
+<a href="/">Go Back</a>
+   
+
+    </body>
+</html>
